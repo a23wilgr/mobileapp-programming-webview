9a25e4f15ffcb5157e638b4157dfc2a7a0805b25
a23wilgr <a23wilgr@student.his.se>
2024-04-10 10:41:26 +0200

    Kallar på external och internal i dropdown

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -18,12 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void showExternalWebPage(){
         myWebView.loadUrl("https://his.se");
-        // TODO: Add your code for showing external web page here
     }
 
     public void showInternalWebPage(){
         myWebView.loadUrl("file:///android_asset/about.html");
-        // TODO: Add your code for showing internal web page here
     }
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -42,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        myWebView.loadUrl("file:///android_asset/about.html");
         myWebView.loadUrl("https://his.se");
+        myWebView.loadUrl("file:///android_asset/about.html");
         /*
         * Rename your App. Tip: Values->Strings
         * Enable Internet access for your App. Tip: Manifest
@@ -87,11 +85,13 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
             Log.d("==>","Will display external web page");
+            showExternalWebPage();
             return true;
         }
 
         if (id == R.id.action_internal_web) {
             Log.d("==>","Will display internal web page");
+            showInternalWebPage();
             return true;
         }
 
